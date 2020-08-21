6cb9a7608df586c349c0223015b39b23c555d99b
cho319 <cho319us@gmail.com>
2020-08-21 01:39:39 -0700

    dockerize service

--- database/index.js
+++ database/index.js
@@ -1,9 +1,18 @@
 const mysql = require('mysql');
 const mysqlConfig = require('./config.js');
 
+// For local host
+// module.exports.connection = mysql.createConnection({
+//   host     : 'localhost',
+//   user     : mysqlConfig.mysqlUser,
+//   password : mysqlConfig.mysqlPW,
+//   database : 'calendar'
+// });
+
+// For docker network
 module.exports.connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : mysqlConfig.mysqlUser,
-  password : mysqlConfig.mysqlPW,
-  database : 'calendar'
+  host     : '172.17.0.2',
+  user     : 'root',
+  database : 'calendar',
+  port     : '3306'
 });
